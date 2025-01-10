d2c44dd685fd7aed6d63b4b2b11f50419b8142cc
dada <dadad@gmail.com>
2025-01-10 17:18:15 +0100

    Initial Commit

---
+++ FireBasePrueba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBasePrueba
+//
+//  Created by Tardes on 10/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
